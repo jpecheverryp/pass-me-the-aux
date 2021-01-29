1a751caaabd68a6b5898829eefd3acb3b8974354
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-01-28 20:27:14 -0500

    added lyrics on page after ajax call

--- assets/main.js
+++ assets/main.js
@@ -1,36 +1,37 @@
-$(document).ready(function() {
-    $(".search").on("click", function (event) {
+$(document).ready(function () {
+    $("#submit-btn").on("click", function (event) {
         event.preventDefault();
-    // songkick API
-    var apiKey = "not sure yet";
-    var queryURL = "https://api.songkick.com/api/3.0/artists/379603/gigography.json?apikey={your_api_key}" + apiKey;
+        // // songkick API
+        // var apiKey = "not sure yet";
+        // var queryURL = "https://api.songkick.com/api/3.0/artists/379603/gigography.json?apikey={your_api_key}" + apiKey;
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+        // $.ajax({
+        //     url: queryUrl,
+        //     method: "GET"
+        // }).then(function (response) {
+        //     console.log(response);
+        // });
+        console.log('test');
+        song = $("#songName").val();
+        artist = $("#artistName").val();
+        queryUrl = "https://api.lyrics.ovh/v1/" + artist + "/" + song + "/";
+        console.log(queryUrl);
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (result) {
+            console.log(result);
+
+            var lyrics = result.lyrics;
+            console.log(lyrics);
+
+            var songTitle = $('#song-title');
+            songTitle.text(song);
+
+            var lyricsEl = $('#lyrics');
+            lyricsEl.text(lyrics)
+        })
     });
-};
+})
 
-$(".btn").on("click", function (e) {
-    e.preventDefault()
-
-    song = $("#songName").val();
-    artist = $("#artistName").val();
-    queryUrl = "https://api.lyrics.ovh/v1/" + artist + "/" + song + "/";
-    console.log(queryUrl);
-
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(result){
-        console.log(result);
-
-        var lyrics = result.lyrics;
-        console.log(lyrics);
-
-        var printLyrics = $("<p>").text(lyrics)
-        $(".overflow").append(printLyrics);
-    })
-});
